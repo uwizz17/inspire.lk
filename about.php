89ffffe86ad0b3ac4fad580089ca40fc276e66c9
uwizz17 <84677917+uwizz17@users.noreply.github.com>
2023-01-30 14:47:32 +0530

    my fisrt commit

---
+++ about.php
@@ -0,0 +1,483 @@
+<?php include 'includes/header.php'; ?>
+
+<section class="body-wrapper"> 
+    <!-- <div class="right-sidebar">
+
+   
+    <div class="button_container" id="toggle">
+    <span class="top"></span>
+    <span class="middle"></span>
+    <span class="bottom"></span>
+    </div>
+
+    <div class="sidebar-menu">
+      <div class="right-img">
+        <img src="img/icons/binocular.png" alt="">
+      </div>
+      <div class="right-img">
+        <img src="img/icons/hiking.png" alt="">
+      </div>
+      <div class="right-img">
+        <img src="img/icons/hotel.png" alt="">
+      </div>
+      <div class="right-img">
+        <img src="img/icons/car.png" alt="">
+      </div>
+      <div class="right-img no-border">
+        <img src="img/icons/open.png" alt="">
+      </div>
+    </div>
+
+    </div> -->
+    
+    <div class="overlay" id="overlay">
+      <nav class="overlay-menu">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Work</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+
+      <section id="sub-page" class="sub-3">
+        <div class="slider-center">
+          <div class="slide-text">
+            <h2>SRI LANKA PACKS A MEAN SERVING OF ADVENTURE </h2>
+            <p class="btn btn-common btn-common-red">Explore Sri Lanka</p>
+          </div>
+       </div>
+      </section> 
+
+      <section>
+        <div class="container-fluid">
+          <div class="row">
+         <div class="col-lg-6">
+          <div class="about-wrapper">
+            <div class="about-header">
+              <span>ABOUT US</span>
+            <h3>About Inspire Holidays (Pvt) Ltd</h3>
+            </div>
+            <p>Welcome to Sri Lanka, the land with exotic beauty surrounded by turquoise water, white
+              sandy, swirls of beaches with the lineup of land mark coconut trees and gorgeous sunset.
+              Inspire Holiday Tours is a truly inspirational travel planner for you and your entire family
+              that vouches to help create ever lasting memories</p>
+            <p>Inaugurated and situated in the heart
+              of Colombo, Sri Lanka is a pioneer in the travel and tour industry, providing the best of
+              services and assistance to anyone who wants a fabulous out of this world experience
+              within the timeline and budget. Their staff lineup has members who have first hand
+              experience and knowledge of handling both individuals and group troop of travelers.</p>
+            <p class="mb-0">The
+              best aspect of Inspiration Tour is their ability to fit all that you want on a sight-seeing
+              with top notch accommodation and cuisine that fits your taste, style and budget. Their
+              attentive staff with assist you outline a travel plan based on your likes and expectation. </p>
+          </div>
+         </div>
+         <div class="col-lg-6 no-padding">
+          <div class="about-right d-flex ">
+            <div class="mb-5">
+              <h4>VISION</h4>
+            <p class="mb-0">Inspire us by inspiring to provide the best travel experience that is possible with an
+              experience of enjoying the comfort of the home being away from home. Enrich the servie
+              by providing the best to each of the traveler that will enable them to travel with us all
+              through, over and over again.</p>
+            </div>
+            <div>
+              <h4>MISSION</h4>
+            <p class="mb-0">We, at Inspire Holiday Tours do look up to cater to all demands and requests of our
+              travelers to give them each a best travel experience and a warm stay, making memories.
+             <!---- <ul>
+                <li>Offering the best possible service to our guests and continuously trying to improve our service</li>
+                <li>Understanding the latest trends and evolving continuously to stay above the competition</li>
+                <li>Use the technology for the best affect to make guest’s experience smoother from start to the end</li>
+                
+              </ul> -->
+            </p>
+            </div>
+          </div>
+         </div>
+        </div>
+        </div>
+      </section>
+
+      <!--what to plan-->
+      <section class="stat-sec">
+        <div class="container">
+          <div class="stat text-center">
+            <h3>Sri Lanka, her beauty and hospitality</h3>
+          </div>
+        <div class="stat-div">
+          <p align="justify">Sri Lanka is well known for its luscious green tea plantations, gushing waterfalls and
+            natural sanctuaries that help protect wildlife of rare breed of birds, plants and animals.;
+            complacent hot tropical weather right through the year, local cultural festivals and
+            celebration makes it more appealing to travel.What inspirers most to return on holidays
+            to Sri Lanka is her warm hospitality, rich diversified culture, historical heritage, century
+            old monuments and the mouth-watering spicy tangy flavoured food besides her beauty. </p>    
+        </div>
+        <div class="stat text-center">
+          <h3>What to plan?</h3>
+        </div>
+        <div class="stat-div">
+          <p align="justify">Inspire Holiday Tours caters to all types of travelers from Group family travelers, group
+            corporate travelers, family holidayers, couples, couples on special event celebrations and
+            individuals. The accommodations can be varied based on the specific needs of the
+            travelers along with cuisine, activities and entertainments. We do adhere to numerous request to organise weddings in both traditional Sri Lankan style to civil ceremonies,
+            anniversary celebrations, birthdays, retirement parties, gender reveal parties to reunion
+            and special get to gethers. There isn’t single event that has been omited or requiest has
+            been ignored. If travelers need a beach based holidays with events such as paragliding,
+            surfing, snorkelling, fishing, deep sea diving, whale watching, a swim with the dolphines
+            to just sit and relax while watching the sunset – all that one needs to do is give our staff
+            the heads up for it to be organised to perfection. Most of the accommodation comes with
+            a mouth watering array of cuisines to feast. Besides that if someone has a vegan, halal,
+            Kosher or other specific alergy free dierary requirements please let our staff know so they
+            can arrange them to give you an edge over the real feel of an experience. Our itinary do
+            cater for children and can be designed to include special age appropriate kid friendly
+            activities.</p> 
+            <p align="justify">We provide the safest, road worthy transprot modes of your choice being a car, travel
+              coach to van depending on the number of travelers. All transport vehicles come with air-
+              conditioning and with safe, comfortable seating facilities. The chaufer assigned are the
+              best in their trade and do know the pathway to your destination thouroughly. If the need
+              be a travel-guide can be arranged to enrich your travel and to make it more informative
+              and exciting.</p>   
+        </div>
+
+        <div class="stat text-center">
+          <h3>Why Inspire Holiday Tours?</h3>
+        </div>
+        <div class="stat-div">
+          <p align="justify">Award winning, best travel and tour company based in Sri Lanka that provides the best
+            travel agenda that fits all your expectation and desire with the most viable ways, creating
+            memories and moments that lasts a life long. Any event or any number of travelers, no
+            problem Inspire Holiday Tours will device an itinerary that caters to all demands and
+            requests of each traveler. Journey with them once and you will be tiying up with them
+            for life.</p>    
+        </div>
+
+
+            
+        </div>
+      </section>
+      <!--end second section-->
+
+      
+
+      <section class="stat-sec-white">
+        <div class="container">
+          <div class="stat text-center">
+            <h3>Statement of Core Values</h3>
+          </div>
+        <div class="stat-div">
+          <p>
+            <ul>
+              <li>Honesty</li>
+              <li>Teamwork to provide the best to our clients</li>
+              <li>Customer centric. We respect all desires and requests of each traveler.</li>
+              <li>Quality service that ensures your journey is pleasant and memorable.</li>
+              <li>Constructive and creative Spirited staff.</li>
+            </ul>
+          </p>
+           
+        </div>
+
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="stat-box-main">
+                  <div class="stat-wrap-left">
+                    <img src="img/stat-icons/solutions.png" alt="">
+                  </div>
+                  <div class="stat-wrap-right">
+                    <h4>Integrity</h4>
+                    <p>We do the right thing even when no one is looking</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="stat-box-main">
+                  <div class="stat-wrap-left">
+                    <img src="img/stat-icons/transparency.png" alt="">
+                  </div>
+                  <div class="stat-wrap-right">
+                    <h4>Transparency</h4>
+                    <p>We are open in all our dealings with all stakeholders</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="stat-box-main">
+                  <div class="stat-wrap-left">
+                    <img src="img/stat-icons/reliability.png" alt="">
+                  </div>
+                  <div class="stat-wrap-right">
+                    <h4>Reliability</h4>
+                    <p>We could be trusted for quality performance</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4">
+                <div class="stat-box-main">
+                  <div class="stat-wrap-left">
+                    <img src="img/stat-icons/responsibility.png" alt="">
+                  </div>
+                  <div class="stat-wrap-right">
+                    <h4>Accountability</h4>
+                    <p>We take responsibility and ownership for all our actions</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4">
+                <div class="stat-box-main">
+                  <div class="stat-wrap-left">
+                    <img src="img/stat-icons/customer-care.png" alt="">
+                  </div>
+                  <div class="stat-wrap-right">
+                    <h4>Care</h4>
+                    <p>We protect our people and honour our commitments</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4">
+                <div class="stat-box-main">
+                  <div class="stat-wrap-left">
+                    <img src="img/stat-icons/friendship.png" alt="">
+                  </div>
+                  <div class="stat-wrap-right">
+                    <h4>Respect</h4>
+                    <p>We value our collective diversity with respect</p>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+        </div>
+      </section>
+
+
+      <section class="our-team">
+        <div class="container">
+          <div class="stat text-center">
+            <h3>Our People</h3>
+          </div>
+          <div class="row">
+            <div class="col-lg-3">
+              <div class="team">
+                <img src="img/user.jpg" class="w-100" alt="">
+                <div class="">
+                  <h4>Jon Doe</h4>
+                  <span>Chief Executive Officer</span>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto consectetur.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="team">
+                <img src="img/user.jpg" class="w-100" alt="">
+                <div class="">
+                  <h4>Jon Doe</h4>
+                  <span>Chief Executive Officer</span>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto consectetur.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="team">
+                <img src="img/user.jpg" class="w-100" alt="">
+                <div class="">
+                  <h4>Jon Doe</h4>
+                  <span>Chief Executive Officer</span>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto consectetur.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="team">
+                <img src="img/user.jpg" class="w-100" alt="">
+                <div class="">
+                  <h4>Jon Doe</h4>
+                  <span>Chief Executive Officer</span>
+                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto consectetur.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- <section class="logos">
+        <div class="container">
+          <div class="row logo-sec">
+            <div class="col-lg-4">
+              <img src="img/logos/slaito.png" alt="">
+            </div>
+            <div class="col-lg-4">
+              <img src="img/logos/wonder-of-asia.png" alt="">
+            </div>
+            <div class="col-lg-4">
+              <img src="img/logos/wspa.png" alt="">
+            </div>
+          </div>
+        </div>
+      </section> -->
+
+      <footer class="footer-bg">
+        <div class="container">
+          <div class="row footer-lg">
+
+            <div class="col-lg-6">
+              <div class="contact-box">
+                <h3>SEND US A MESSAGE</h3>
+                <form>
+                  <div class="mb-3">
+                    <input type="email" class="form-control" placeholder="Your Name">
+                  </div>
+                  <div class="mb-3">
+                    <input type="password" class="form-control" placeholder="Your Email">
+                  </div>
+                  <div class="mb-3">
+                    <textarea class="form-control" rows="3" placeholder="Message"></textarea>
+                  </div>
+                 
+                  <button type="submit" class="btn btn-readmore">Send</button>
+                </form>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="footer-content-1">
+                <h3>TALK TO US</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto labore harum cumque impedit, consequatur eveniet quos numquam. Quos dolorum deleniti rem repellat praesentium quis, sit omnis deserunt.</p>
+                <div class="footer-contact d-flex">
+                
+                  <p><i class="fas fa-phone-alt"></i> +94 77 304 1595</p>
+                  <p><i class="far fa-envelope"></i> info@seasonsholidays.lk</p>
+                 </div>
+              <div class="social-icons">
+                <img src="img/social-icons/facebook.png" alt="">
+                <img src="img/social-icons/twitter.png" alt="">
+                <img src="img/social-icons/youtube.png" alt="">
+                <img src="img/social-icons/instagram.png" alt="">
+              </div>
+              </div>
+            </div>
+
+
+            <div class="col-lg-12">
+              <p class="text-center mb-0 copyright">Copyright ©2022 Inspire Holidays (Pvt) Ltd. Web Design By <a href="">Web Design Sri Lanka</a></p>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    </section>
+     
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script>
+      $(document).ready(function(){
+      var $prev = $('.previousx');
+      var $next = $('.nextx');
+      var mode = "auto";
+      $prev.on({
+          click: function(e){
+          e.preventDefault();
+          mode = "auto";
+          showPreviousImage();
+          }
+      });
+      $next.on({
+          click: function(e){
+          e.preventDefault();
+          mode = "auto";
+          showNextImage();
+          }
+      });
+      setInterval(function(){
+          if(mode==="auto"){
+          showNextImage();
+          }
+      },3000);
+      function showNextImage(){
+          var $actEl = $('.activx');
+          var $nextEl = $actEl.next('.slidex');
+          if($nextEl.length){
+              $actEl.removeClass('activx');
+              $nextEl.addClass('activx');
+          }else{
+              $actEl.removeClass('activx');
+              $('.slidex:first-child').addClass('activx');
+          }
+      }
+      function showPreviousImage(){
+          var $actEl = $('.activx');
+          var $prevEl = $actEl.prev('.slidex');
+          if($prevEl.length){
+              $actEl.removeClass('activx');
+              $prevEl.addClass('activx');
+          }else{
+              $actEl.removeClass('activx');
+              $('.slidex.last').addClass('activx');
+          }
+          }
+      });
+  </script>
+<script>
+  $('#toggle').click(function() {
+   $(this).toggleClass('active');
+   $('#overlay').toggleClass('open');
+  });
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+
+<script>
+  $('.popular-places').owlCarousel({
+    loop:true,
+    margin:5,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    nav:true,
+    navText : ['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+</script>
+<script>
+  $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+     //>=, not <=
+    if (scroll >= 50) {
+        //clearHeader, not clearheader - caps H
+        $(".nav-link-wrapper").addClass("out");
+    }
+    else {
+      $(".nav-link-wrapper").removeClass("out");
+    }
+});
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.ripples/0.5.3/jquery.ripples.js"> </script>
+<script>
+  $('.popular-tours-wrapper').ripples({
+      resolution: 512,
+      dropRadius: 20,
+      perturbance: 0.04,
+      });
+</script>
+  </body>
+</html>
